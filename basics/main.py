0b4e7f8048402bf3734ee8859e439f1a0e8ade84
Narayanam Dakshayani <narayanamdakshayani04@gmail.com>
2026-02-24 14:46:00 +0530

    Initail commit

---
+++ basics/main.py
@@ -0,0 +1,10 @@
+# Exponentation represented by **
+print(3**6)
+# Remainder is obtained when % is used 
+print(51 % 7)
+# // means integer division is same as the regular division but the result is rounded down.
+print(26//5)
+# / is division
+print(23 / 5)
+# *,-,+ are multiplication , subtraction and addition respectively.
+print(2+7)
